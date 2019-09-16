0044e2ed088e7f92ccd9cc73d2a673c7d493d9c5
giocarvalho07 <giovannidecarvalhomendes@outlook.com>
2019-09-15 23:10:01 -0300

    criar movimento da cobrinha - projeto jogo da cobrinha js

--- script.js
+++ script.js
@@ -3,11 +3,18 @@ let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
 // seleciona o arquivo dando a ele uma visão em 2d
 let box = 32;
+
+// ---------  array cobrinha ----
 let snake = [];
 snake[0] = {
     x: 8 * box,
     y: 8 * box
 }
+
+// ---------  array cobrinha ----
+
+let direction = "rigth";
+
 
 function criarBG() {
     context.fillStyle = "lightgreen";
@@ -25,7 +32,28 @@ function criarCobrinha() {
 }
 // Para criar a cobrinha ela usa uma lógica de acrescer um item no array e retirar o último elemento fazendo assim que ela consiga andar
 
+function iniciarJogo() {
+    criarBG();
+    criarCobrinha();
 
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
 
-criarBG();
-criarCobrinha();
+    if (direction == "rigth") snakeX += box;
+    if (direction == "left") snakeX -= box;
+    if (direction == "up") snakeY -= box;
+    if (direction == "down") snakeY += box;
+
+    snake.pop();
+
+    let newHead = {
+        x: snakeX,
+        y: snakeY
+    }
+
+    snake.unshitft(newHead);
+
+}
+
+let jogo = setInterval(iniciarJogo, 100);
+// paramentro Interval - para dar movimentação e atualizar o jogo a cada milisegundo tornando assim a cobrinha movimentada
